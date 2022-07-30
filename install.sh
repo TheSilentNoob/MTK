05f7c0cbcc29f3e7a10c918a2c85f268ccf707e5
TheSilentNoob <99089607+TheSilentNoob@users.noreply.github.com>
2022-07-30 15:28:44 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ git clone git@github.com:TheSilentNoob/Fast-Message.git
 mv Fast-Message core
 git clone git@github.com:TheSilentNoob/BlueSpoof.git
 mv BlueSpoof core
-git clone git@github.com:TheSilentNoob/ARP_MAC_Spoofing.git
+git clone git@github.com:TheSilentNoob/AM_Spoofing.git
 mv ARP_MAC_Spoofing core
